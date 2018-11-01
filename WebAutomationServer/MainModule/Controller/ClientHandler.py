99179138a4b639bce26de7817a9e1fb8f7b2ef3e
Iceyo Kuna <iceyokuna@hotmail.com>
2018-11-01 13:29:39 +0700

    update[model]

--- WebAutomationServer/MainModule/Controller/ClientHandler.py
+++ WebAutomationServer/MainModule/Controller/ClientHandler.py
@@ -3,5 +3,17 @@ class ClientHandler:
         self.main_user_list = []
         self.end_user_list = []
     
-    def addMainUser(self):
-        pass
+    def addMainUser(self, client):
+        self.main_user_list.append(client)
+        print(self.main_user_list)
+    
+    def addEndUser(self, client):
+        self.end_user_list.append(client)
+        print(self.end_user_list)
+
+    def removeMainUser(self, client):
+        self.main_user_list.remove(client)
+
+    def removeEndUser(self, client):
+        self.end_user_list.remove(client)
+        
