eb36b9b034b82ceb7fded10071f5af594de4e5c5
treesakul <treesakul@hotmail.com>
2018-11-01 21:41:54 +0700

    update DB

--- AccountManagement/AccountManagement/settings.py
+++ AccountManagement/AccountManagement/settings.py
@@ -91,9 +91,13 @@ WSGI_APPLICATION = 'AccountManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wasdb',
+        'USER': 'wasadmin',
+        'PASSWORD': 'wasadmin',
+        'HOST': 'cair.p-enterprise.com',
+        'PORT': '5432',
+        }
 }
 
 
