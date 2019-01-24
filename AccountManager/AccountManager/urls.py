bdfc5c18e2f1669343d4d57a83613a6c8e123b00
treesakul <treesakul@hotmail.com>
2019-01-24 17:18:57 +0700

    Rewrite AccountManager to RestAPI

--- AccountManager/AccountManager/urls.py
+++ AccountManager/AccountManager/urls.py
@@ -1,4 +1,4 @@
-"""AccountManagement URL Configuration
+"""AccountManager URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from APIModule.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/login', login)
 ]
--- AccountManager/AccountManager/wsgi.py
+++ AccountManager/AccountManager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AccountManagement project.
+WSGI config for AccountManager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AccountManagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AccountManager.settings')
 
 application = get_wsgi_application()
