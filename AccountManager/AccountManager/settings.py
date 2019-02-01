69e9333594f01e60c2d4ff21d86397a300ba050a
Iceyo Kuna <iceyokuna@hotmail.com>
2019-02-01 15:40:30 +0700

    [Update] test protgres [account manager]

--- AccountManager/AccountManager/settings.py
+++ AccountManager/AccountManager/settings.py
@@ -84,14 +84,25 @@ WSGI_APPLICATION = 'AccountManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+'''
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wasdb',
-        'USER': 'postgres',
+        'USER': 'was',
         'PASSWORD': '123159',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
