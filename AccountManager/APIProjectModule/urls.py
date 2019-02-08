0c07213b6b46c658392abe351dfefd3ed38c0b7d
treesakul <treesakul@hotmail.com>
2019-02-08 18:37:27 +0700

    Uppdate workflow API

--- AccountManager/APIProjectModule/urls.py
+++ AccountManager/APIProjectModule/urls.py
@@ -2,13 +2,16 @@ from django.urls import path, include, reverse
 from . import views
 from rest_framework import routers
 
-routers = routers.DefaultRouter()
+
+from APIProjectModule.views import WorkflowView
+#routers = routers.DefaultRouter()
+
 #routers.register('admin', views.AdminView)
-routers.register('workflow', views.WorkflowView)
+#routers.register('workflow', views.WorkflowView.as_view())
 #routers.register('collaborator', views.CollaboratorView)
 
 urlpatterns = [
-    
-    path('', include(routers.urls))
+    path('api/workflow', WorkflowView.as_view())
+    #path('', include(routers.urls))
     
 ]
--- AccountManager/APIProjectModule/views.py
+++ AccountManager/APIProjectModule/views.py
@@ -2,11 +2,31 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Workflow #, Admin, Collaborator
 from .serializers import WorkflowSerializer #, AdminSerializer, CollaboratorSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+from rest_framework.response import Response
 
 # Create your views here.
-class WorkflowView(viewsets.ModelViewSet):
-    queryset = Workflow.objects.all()
-    serializer_class = WorkflowSerializer
+@permission_classes((IsAuthenticated,))
+class WorkflowView(APIView):
+   
+    def get(self, request):
+        queryset = Workflow.objects.filter(user= request.user.username).values()
+        return Response({'workflow':queryset},status=HTTP_200_OK)
+
+    def post(self, request):
+        Workflow.objects.create(user= request.user, workflow = request.POST.get('workflow'), name = request.POST.get('name'))
+        return Response(status=HTTP_200_OK)
+
+    
+  
+
 '''
 class AdminView(viewsets.ModelViewSet):
     queryset = Admin.objects.all()
