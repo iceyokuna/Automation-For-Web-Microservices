58420f7edc227008eb25c9a794486899094017ef
treesakul <treesakul@hotmail.com>
2019-02-15 16:55:18 +0700

    Fix Email

--- EmailService/EmailAPI/views.py
+++ EmailService/EmailAPI/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 #rest framework
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
@@ -12,25 +13,19 @@ from rest_framework.status import (
     HTTP_201_CREATED
 )
 from rest_framework.response import Response
-from django.template.loader import render_to_string
-from mail_templated import send_mail
-from rest_framework.permissions import AllowAny
-#from .Templates import email
 # Create your views here.
-from rest_framework.permissions import  IsAuthenticated
-from django.core.mail import EmailMultiAlternatives
 
-#@permission_classes((AllowAny,))
+
+@permission_classes((AllowAny,))
 class EmailView(APIView):
     def post(self, request):
-        
         subject = request.data.get('subject')
         message = request.data.get('message')
         recipient_list = request.data.get('email')
         email_from = settings.EMAIL_HOST_USER
 
-        if(send_mail( subject, message, email_from, [recipient_list] )):
+        #if(send_mail( subject, message, email_from, [recipient_list] )):
+        if(send_mail( subject, message, email_from, recipient_list )):
             return Response(request.data, status=HTTP_200_OK)
-        
 
         return Response( status=HTTP_400_BAD_REQUEST)
