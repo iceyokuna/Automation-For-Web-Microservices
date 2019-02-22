4d77665cf5718df9d77d16926e5eac4fc8f6d38c
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-02-22 18:40:05 +0700

    - Update default email form - Add id to textarea in the form

--- Client/src/containers/BpmnBoard/components/bpmn_property/index.js
+++ Client/src/containers/BpmnBoard/components/bpmn_property/index.js
@@ -73,11 +73,8 @@ class BpmnProperty extends Component {
   renderSpecialProperties() {
     const { nodeType, isAsyncTask } = this.state;
     const { allServices, onSelectServiceMethod } = this.props;
-
+    console.log(allServices);
     // const services = allServices.length == 0 ? services : allServices;
-    // const services = services;
-
-    let elements = null;
 
     switch (nodeType) {
       // case 'bpmn:Task': {
