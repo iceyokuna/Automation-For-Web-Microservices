269c34b0054a7cae06b9f78d7745831799968a36
treesakul <treesakul@hotmail.com>
2019-03-12 19:01:54 +0700

    update add memember

--- AccountManager/APIProjectModule/models.py
+++ AccountManager/APIProjectModule/models.py
@@ -22,7 +22,7 @@ class Admin(models.Model):
 '''
 class Collaborator(models.Model):
     workflow = models.ForeignKey(Workflow, related_name='project_col', on_delete=models.CASCADE)
-    Collaborator = models.ForeignKey(User, related_name='collaborator', on_delete=models.CASCADE)
+    collaborator = models.ForeignKey(User, related_name='collaborator', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.workflow.name
