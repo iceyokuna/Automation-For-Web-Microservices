431231d9fca19893d2b3e17c2468e62ddcfc6775
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-03-19 16:10:08 +0700

    Complete my_tasks page

--- Client/src/components/task_item_inbox/index.js
+++ Client/src/components/task_item_inbox/index.js
@@ -3,19 +3,20 @@ import React from 'react'
 import { Box, Text, Button } from 'grommet';
 import { Checkmark, Close } from 'grommet-icons'
 
-const index = ({ ...props }) => {
+const index = ({ onApprove, onReject, isEven, ...props }) => {
   return (
-    <Box round={{ size: "small" }} direction="row" background="light-0" pad="xsmall" height="40px">
-      <Box flex={{ grow: 2 }} align="center" justify="center">
-        <Text textAlign="center">Test</Text>
+    <Box round={{ size: "small" }} direction="row"
+      background={isEven === true ? "light-0" : "#FFEBEB"} pad="xsmall" height="40px">
+      <Box style={{ flex: 2 }} align="center" justify="center">
+        <Text weight="bold">{props.workflowName}</Text>
       </Box>
-      <Box flex={{ grow: 6 }} align="center" justify="center" >
-        <Text textAlign="center">Test</Text>
+      <Box style={{ flex: 6 }} align="center" justify="start" direction="row" gap="small">
+        <Text weight="bold">[{props.actionType}]</Text>
+        <Text >{props.actionDescription}</Text>
       </Box>
-      <Box flex={{ grow: 1 }} direction="row" align="center" justify="center" >
-        <Text textAlign="center">Test</Text>
-        <Button icon={<Checkmark />} />
-        <Button icon={<Close />} />
+      <Box style={{ flex: 1 }} direction="row" align="center" justify="center" >
+        <Button onClick={() => onApprove()} icon={<Checkmark color="#509137" />} />
+        <Button onClick={() => onReject()} icon={<Close color="#F04B37" />} />
       </Box>
     </Box>
   )
--- Client/src/pages/my_tasks/index.js
+++ Client/src/pages/my_tasks/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import {
   Box, Button,
-  Heading,
+  Heading, Text
 } from 'grommet';
 
 import { Add } from 'grommet-icons';
@@ -11,10 +11,43 @@ import { global } from 'style';
 
 import TaskItemInbox from 'components/task_item_inbox';
 
-export default class MyTasks extends Component {
-  renderTasks = () => {
+import axios from 'axios';
 
-    return ([<TaskItemInbox />, <TaskItemInbox />]);
+export default class MyTasks extends Component {
+
+  state = {
+    myTasks: [],
+  }
+
+  componentDidMount = () => {
+    axios.get('https://5c8f65998447f30014cb826b.mockapi.io/api/endUserTasks').
+      then(res => {
+        this.setState({
+          myTasks: res.data
+        })
+      }).catch(err => {
+        console.error(err);
+      })
+  }
+
+  onApproveTask = (item) => {
+
+  }
+
+  onRejectTask = (item) => {
+
+  }
+
+  renderTasks = () => {
+    const { myTasks } = this.state;
+
+    return myTasks.map((item, index) =>
+      <TaskItemInbox workflowName={item.workflowName}
+        isEven={index % 2 == 0}
+        actionType={item.actionType}
+        actionDescription={item.actionDescription}
+        onApprove={() => this.onApproveTask(item)}
+        onReject={() => this.onRejectTask(item)} />);
   }
 
 
@@ -38,6 +71,17 @@ export default class MyTasks extends Component {
         </Box>
 
         <Box direction="column" gap="small">
+          <Box direction="row" align="center">
+            <Box style={{ flex: 2 }}>
+              <Text textAlign="center" weight="bold">Workflow</Text>
+            </Box>
+            <Box style={{ flex: 6 }}>
+              <Text textAlign="center" weight="bold">Action</Text>
+            </Box>
+            <Box style={{ flex: 1 }}>
+              <Text textAlign="center" weight="bold">Date</Text>
+            </Box>
+          </Box>
           {this.renderTasks()}
         </Box>
 
