47c26f6bb22d3074f3ce7f9c863e83f9a24e2469
treesakul <treesakul@hotmail.com>
2019-03-25 21:42:25 +0700

    Update urls.py

--- ServiceRegistry/APIModule/urls.py
+++ ServiceRegistry/APIModule/urls.py
@@ -3,9 +3,9 @@ from . import views
 from rest_framework import routers
 
 routers = routers.DefaultRouter()
-routers.register('all_services/', views.AllServicesView)
-routers.register('services/', views.ServiceView)
-routers.register('methods/', views.MethodView)
+routers.register('all_services', views.AllServicesView)
+routers.register('services', views.ServiceView)
+routers.register('methods', views.MethodView)
 routers.register('connecting_methods/(?P<m_id>\d+)', views.ConnectingMethodView)
 
 urlpatterns = [
