68866d460cb4e89671fa8efeaeca084b1ea6c0e4
Iceyo Kuna <iceyokuna@hotmail.com>
2019-03-26 02:34:43 +0700

    [UPDATE] plan for parallel execution

--- WebAutomationServer/MainModule/Graphflow/WorkflowEngine.py
+++ WebAutomationServer/MainModule/Graphflow/WorkflowEngine.py
@@ -79,12 +79,18 @@ class WorkflowEngine:
         return (element_object.getHTML())
         
     def next(self):
+        #get object from next transition
         self.currentState["current"] = self.transition[(self.currentState["current"],"")]
+
+        #check that is end state or not
         if(self.currentState["current"] in self.endState):
             #DEBUG_LOG_WHEN_EXECUTION_DONE
             self.showLog()
             return "DONE"
+        
+        #Tasks case return HTML and perform services (2 cases have/not have form)
         element_object = self.state[self.currentState["current"]]
+
         return (element_object.getHTML())
 
     def setUserInput(self, userInput):
