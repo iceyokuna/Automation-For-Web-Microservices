a60b864f4ec7125ef2a3f3d00df32d5284847120
treesakul <treesakul@hotmail.com>
2019-03-26 03:07:54 +0700

    Update models.py

--- AccountManager/APIProjectModule/models.py
+++ AccountManager/APIProjectModule/models.py
@@ -7,9 +7,11 @@ class Workflow(models.Model):
     #id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     workflow = JSONField(null=False)
+    description = models.TextField(null=True)
     user = models.ForeignKey(User, to_field="username", db_column="username", related_name='project_user', on_delete=models.CASCADE)
     appliedMethod = JSONField(null=True)
     appliedConditions = JSONField(null=True)
+    generatedForm = JSONField(null=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +23,7 @@ class Admin(models.Model):
     
     #def __str__(self):
     #    return self.name
-'''
+
 
 class Task(models.Model):
     workflow = models.ForeignKey(Workflow, related_name='project_task', on_delete=models.CASCADE)
@@ -29,7 +31,7 @@ class Task(models.Model):
 class Condition(models.Model):
     workflow = models.ForeignKey(Workflow, related_name='project_condition', on_delete=models.CASCADE)
     condition = models.TextField(null=True) #what columns do you want to add?
-
+'''
 class Collaborator(models.Model):
     workflow = models.ForeignKey(Workflow, related_name='project_col', on_delete=models.CASCADE)
     collaborator = models.ForeignKey(User, related_name='collaborator', on_delete=models.CASCADE)
