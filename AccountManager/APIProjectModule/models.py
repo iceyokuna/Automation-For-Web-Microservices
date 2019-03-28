aab04248f56de41635eab537b0ec95aee62491c5
treesakul <treesakul@hotmail.com>
2019-03-28 19:24:21 +0700

    Update models.py

--- AccountManager/APIProjectModule/models.py
+++ AccountManager/APIProjectModule/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
-
+'''
 # Create your models here.
 class Workflow(models.Model):
     #id = models.AutoField(primary_key=True)
@@ -13,12 +13,11 @@ class Workflow(models.Model):
     appliedConditions = JSONField(null=True)
     appliedPreInputs = JSONField(null=True)
     generatedForms = JSONField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.name
-'''
+
 class Admin(models.Model):
     #id = models.AutoField(primary_key=True)
     workflow = models.CharField(max_length=200)
@@ -34,10 +33,12 @@ class Task(models.Model):
 class Condition(models.Model):
     workflow = models.ForeignKey(Workflow, related_name='project_condition', on_delete=models.CASCADE)
     condition = models.TextField(null=True) #what columns do you want to add?
-'''
+
 class Collaborator(models.Model):
     workflow = models.ForeignKey(Workflow, related_name='project_col', on_delete=models.CASCADE)
     collaborator = models.ForeignKey(User, related_name='collaborator', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.workflow.name
+
+'''
