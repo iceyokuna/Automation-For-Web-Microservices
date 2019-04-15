c10915c72d456d4a97baa87ab2c424f829b4eb11
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-15 15:57:44 +0700

    [UPDATE] getter

--- WebAutomationServer/MainModule/Graphflow/Core/CoreElement.py
+++ WebAutomationServer/MainModule/Graphflow/Core/CoreElement.py
@@ -4,6 +4,12 @@ class CoreElement:
     def __init__(self, elementId, elementName):
         self.id = elementId
         self.name = elementName
+
+    def getId(self):
+        return self.id
+    
+    def getName(self):
+        return self.name
         
     def __str__(self):
         return str(self.id) + " " + str(self.name)
