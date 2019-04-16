ad483f417da8b203be1d81ff499a950024d6c2bd
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-17 02:05:00 +0700

    [Update] exclusive gate way

--- WebAutomationServer/MainModule/Graphflow/Core/ExclusiveGateway.py
+++ WebAutomationServer/MainModule/Graphflow/Core/ExclusiveGateway.py
@@ -4,14 +4,17 @@ from MainModule.Graphflow.Core.IOtypes import *
 class ExclusiveGateway(Gateway):
     def __init__(self , id, name, inputType , outputType):
         super().__init__(id, name, inputType , outputType)
+        self.flowReferenceList = []
+        self.condition = None
+    
+    def setCondition(self, conditionRef):
+        self.condition = conditionRef
+
+    def calculateCondition(self):
+        pass
 
     #Overiding 
     def getFlowReference(self):
-        selectedFlow = None
-        for flow in self.flowReferenceList:
-            target = flow.getTarget()
-            if(self.inputType == target.getOutputType()):
-                if(flow.checkCondition(self.input)):
-                    return flow
-        #return default flow
-        return self.flowReferenceList[0]
+        if(self.condition is None):
+            return self.flowReferenceList[0]
+        return self.calculateCondition()
