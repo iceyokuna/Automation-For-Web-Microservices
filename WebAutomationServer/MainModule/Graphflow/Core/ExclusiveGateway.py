ca684ac50652ccecbbfc87bd944b0b3c104fbcab
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-17 04:09:34 +0700

    [UPDATE] condition execution done

--- WebAutomationServer/MainModule/Graphflow/Core/ExclusiveGateway.py
+++ WebAutomationServer/MainModule/Graphflow/Core/ExclusiveGateway.py
@@ -6,15 +6,33 @@ class ExclusiveGateway(Gateway):
         super().__init__(id, name, inputType , outputType)
         self.flowReferenceList = []
         self.condition = None
+
+    def addFlowReference(self, elementId):
+        self.flowReferenceList.append(elementId)
     
     def setCondition(self, conditionRef):
         self.condition = conditionRef
 
-    def calculateCondition(self):
-        pass
+    def getRequiredTasks(self):
+        required_task = []
+        if(self.condition is None):
+            return []
+        for condition in self.condition:
+            required_task.append(condition['variable1']['variableOf']['methodOfTaskId'])
+        return required_task
 
-    #Overiding 
-    def getFlowReference(self):
+    #return flow that make condition true
+    def getFlowReference(self, required_task):
+        required_task_dict = required_task
         if(self.condition is None):
             return self.flowReferenceList[0]
-        return self.calculateCondition()
+
+        for condition in self.condition:
+            if(condition['operator'] == "=="):
+                var1Task_object = required_task_dict[condition['variable1']['variableOf']['methodOfTaskId']]
+                var1_value = var1Task_object.getInput()[condition['variable1']['name']]['value']
+                var2_value = condition['variable2']['value']
+                if(var1_value == var2_value):
+                    return condition['targetNode']    
+
+        return self.flowReferenceList[0] #fail to getflow return None (self.flowReferenceList[0] for debuging)
