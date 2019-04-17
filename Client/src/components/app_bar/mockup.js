f803fdcebb66ebf6ed829d2f7ce98465e95d4a31
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-04-17 15:49:53 +0700

    Rename variable

---
+++ Client/src/components/app_bar/mockup.js
@@ -0,0 +1,5 @@
+export const notifications = [
+  { title: "Fill this form", body: "This form is about Registration", createdAt: "2019/3/26", },
+  { title: "Submit your shirt's size", body: "There are S, M, L, XL", createdAt: "2019/3/23", },
+  { title: "Give me your email address", body: "Pls send it by noon", createdAt: "2019/3/22", },
+]
