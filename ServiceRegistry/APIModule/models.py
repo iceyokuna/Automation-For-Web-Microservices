9db17bd81ccd49d9aad05ec0347cb5568fba4bb1
treesakul <treesakul@hotmail.com>
2019-05-01 19:07:02 +0700

    Update models.py

--- ServiceRegistry/APIModule/models.py
+++ ServiceRegistry/APIModule/models.py
@@ -19,8 +19,8 @@ class UserService(models.Model):
     name = models.CharField(max_length=200)
     info = models.TextField(null=True)
     url = models.TextField(null=True)
-    #created = models.DateTimeField(auto_now_add=True)
-    #updated_at = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
