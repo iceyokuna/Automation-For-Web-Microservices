52d9a592fd45b6c7abf99d12b586a26f02ca17cf
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-02 22:05:05 +0700

    [UPDATE] add background process

--- EventManager/EventManager.py
+++ EventManager/EventManager.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 #firebase header
 
+#ping firebase server to check time event (to trigger time event)
+def checkTimeEvent():
+    pass
+
 @app.route('/')
 def hello():
     return "Hello World!"
@@ -15,7 +19,7 @@ def timeEvent():
 #Recieve meesage event (binding)
 @app.route('/messageEvent', methods=['POST'])
 def messageEvent():
-    return "Message Event!"
+    return "Message Event!"'
 
 if __name__ == '__main__':
     app.run()
