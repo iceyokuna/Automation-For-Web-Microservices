fa1de26d0c2d7cc6a7d2a7c56d1e653a4b7a030b
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-02 19:15:17 +0700

    [UPDATE] event manager time&message event

--- EventManager/EventManager.py
+++ EventManager/EventManager.py
@@ -1,10 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+#firebase header
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/timeEvent')
+def timeEvent():
+    return "Time Event!"
+
+@app.route('/messageEvent')
+def messageEvent():
+    return "Message Event!"
 
 if __name__ == '__main__':
     app.run()
