dd004f67634053af16436cefc5b49c94d62b1a04
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-02 18:16:57 +0700

    [UPDATE] flask template for event manager

--- EventManager/EventManager.py
+++ EventManager/EventManager.py
@@ -1,3 +1,10 @@
-class EventManager:
-    def __init__(self):
-        pass
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
