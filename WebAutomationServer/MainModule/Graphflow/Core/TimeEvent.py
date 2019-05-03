932065e2a16a3e787f4fc6692b3bfd785e7532e9
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-03 19:34:25 +0700

    [UPDATE] set time

--- WebAutomationServer/MainModule/Graphflow/Core/TimeEvent.py
+++ WebAutomationServer/MainModule/Graphflow/Core/TimeEvent.py
@@ -1,7 +1,7 @@
 from MainModule.Graphflow.Core.IntermediateEvent import *
 from MainModule.Graphflow.Core.IOtypes import *
 
-class TimeEvent(IntermediateEvent):
+class   (IntermediateEvent):
     def __init__(self , id, name, inputType , outputType, eventDefination):
         super().__init__(id, name, inputType , outputType)
         self.eventDefination = eventDefination
@@ -12,23 +12,20 @@ class TimeEvent(IntermediateEvent):
         self.date = date
         self.time = time
 
+    def getTriggerDate(self):
+        return self.date
+
+    def getTriggerTime(self):
+        return self.time
+
     def setEventDefination(self, defination):
         self.eventDefination = defination
 
     def getEventDefination(self):
         return self.eventDefination
-    
-    def initiate(self):
-        pass
 
     def setDefination(self, eventDefination):
         self.eventDefination = eventDefination
 
     def getDefination(self):
         return self.eventDefination
-
-    def isReady(self):
-        pass
-
-    def getRemainingTime(self):
-        pass
