f42fb3f2582d685ac49e8efc8cbb11617479f2c7
treesakul <treesakul@hotmail.com>
2019-05-06 22:22:54 +0700

    Update ServiceObj.py

--- ServiceRegistry/APICallServiceModule/ServiceObj.py
+++ ServiceRegistry/APICallServiceModule/ServiceObj.py
@@ -32,12 +32,12 @@ class ServiceObj:
 
         if(method_type == 'get' ):
             response = requests.get(url, json = data, headers=self.headers)
-            return Response({'data': response.json()}, status = HTTP_200_OK)
+            return Response({'data': response}, status = HTTP_200_OK)
 
         
         elif(method_type == 'post' ):
             response = requests.post(url, json = data, headers = self.headers)
-            return Response({'data':  response.json()}, status = HTTP_200_OK)
+            return Response({'data':  response}, status = HTTP_200_OK)
 
         else:
             return Response({'detail': method_type},
