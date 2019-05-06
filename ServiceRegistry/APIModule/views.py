cd7e09d2bc721514e569633dd534dce6393f6b9b
treesakul <treesakul@hotmail.com>
2019-05-06 14:23:07 +0700

    delete service and method

--- ServiceRegistry/APIModule/views.py
+++ ServiceRegistry/APIModule/views.py
@@ -141,7 +141,23 @@ class UserMethodView(APIView):
                 return Response({"detail":"method sucessfully created"},status = HTTP_200_OK)
         return Response({"detail":"Unable to create the method"},status = HTTP_200_OK)
 
-      
+    def delete(self, request):
+        url = settings.AUTHENTICATION +'/api/validate_token'
+        headers =  { "Authorization" : request.META.get('HTTP_AUTHORIZATION')}
+        response = requests.get(url, headers=headers)
+        if(json.loads(response.content)['username']):
+            username = json.loads(response.content)['username']
+        else:
+            return Response({"detail":  "User unauthorized"}, status=HTTP_400_BAD_REQUEST) 
+
+        if(request.data.get('service_id')):
+            owner = UserService.objects.filter(id=request.data.get('service_id')).values('username')
+            if(username == owner[0].get('username')):
+                service = UserMethod.objects.filter(id=request.data.get('service_id')).delete()
+                return Response({"detail": "successfully deleted by "+username}, status=HTTP_200_OK)
+            else:
+                return Response({"detail": request.user.username+" does not have access to the service"}, status=HTTP_200_OK)
+
 class AllUserServiceView(viewsets.ModelViewSet):
     queryset = UserService.objects.all()
     serializer_class = AllUserServicesSerializer
