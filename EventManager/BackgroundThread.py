1b6d78d3b66aeb3498f73918ad9cca9566650560
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-06 15:53:28 +0700

    [UPDATE] check date time

--- EventManager/BackgroundThread.py
+++ EventManager/BackgroundThread.py
@@ -1,6 +1,7 @@
 from flask import request
 import threading
 import time
+import datetime
 
 class BackgroundThread(threading.Thread):
     def __init__(self, threadID, name, messager):
@@ -24,16 +25,30 @@ class BackgroundThread(threading.Thread):
 
     def getTimeTriggered(self, data):
         time_trigger_id_list = []
-
+        #get local time
         localtime = time.localtime(time.time())
-        day = localtime.tm_mday
-        month = localtime.tm_mon
-        year = localtime.tm_year
-        hour = localtime.tm_hour
-        minute =localtime.tm_min
+        day_local = localtime.tm_mday
+        month_local = localtime.tm_mon
+        year_local = localtime.tm_year
+        hour_local = localtime.tm_hour
+        minute_local =localtime.tm_min
+        time_system = datetime.datetime(year_local, month_local, day_local, hour_local, minute_local)
+        #check timeevent data in firebase
         for event in data:
-            pass
+            #extract time from event in firebase
+            date_data = data[event]["trigger_date"].split('/')
+            time_data = data[event]["trigger_time"].split(':')
+            day_event= date_data[1]
+            month_event = date_data[0]
+            year_event = date_data[2]
+            hour_event = time_data[0]
+            minute_event =time_data[1]
+            time_event = datetime.datetime(year_event, month_event, day_event, hour_event, minute_event)
+            #check triggering
+            if(time_system > time_event):
+                time_trigger_id_list.append(event)
+        #return list of element_id that are triggered
+        return time_trigger_id_list
 
-
-    def triggerTimeEvent(self, data):
+    def triggerTimeEvent(self, trigger_list):
         pass
