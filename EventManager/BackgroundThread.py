902b7563c39ba63f4ac34f420d829d1724cd615c
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-06 15:25:18 +0700

    [UPDATE] get triggered time event list

--- EventManager/BackgroundThread.py
+++ EventManager/BackgroundThread.py
@@ -3,18 +3,37 @@ import threading
 import time
 
 class BackgroundThread(threading.Thread):
-   def __init__(self, threadID, name, messager):
-       threading.Thread.__init__(self)
-       self.threadID = threadID
-       self.name = name
-       self.messager = messager
+    def __init__(self, threadID, name, messager):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.messager = messager
 
-   def run(self):
-       try:
-           while(True):
-               time_event_data = self.messager.get("TimeEvent",None)
-               print(time_event_data)
-               print("fetch time event successfully!!\n")
-               time.sleep(10)
-       except KeyboardInterrupt:
-           print("Interrupted!")
+    def run(self):
+        try:
+            #fetch event every 10 second
+            while(True):
+                time.sleep(10)
+                time_event_data = self.messager.get("TimeEvent",None)
+                #check and trigger event
+
+                print(time_event_data)
+                print("fetch time event successfully!!\n")
+        except KeyboardInterrupt:
+            print("Interrupted!")
+
+    def getTimeTriggered(self, data):
+        time_trigger_id_list = []
+
+        localtime = time.localtime(time.time())
+        day = localtime.tm_mday
+        month = localtime.tm_mon
+        year = localtime.tm_year
+        hour = localtime.tm_hour
+        minute =localtime.tm_min
+        for event in data:
+            pass
+
+
+    def triggerTimeEvent(self, data):
+        pass
