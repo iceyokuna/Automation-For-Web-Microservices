0351e7703050564ff657a7509c01cbef0e56529e
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-05-10 17:18:24 +0700

    Highlight current applied service

--- Client/src/components/service_list/index.js
+++ Client/src/components/service_list/index.js
@@ -4,31 +4,37 @@ import { Box, Text, Accordion, AccordionPanel, Button } from 'grommet'
 import { FormDown } from 'grommet-icons'
 import PlainButton from 'components/plain_button'
 
-import { Scrollbars } from 'react-custom-scrollbars'
+import { Scrollbars } from 'react-custom-scrollbars';
+import { connect } from 'react-redux';
 
-export default class index extends Component {
+class index extends Component {
 
   state = {
     activeIndex: [0],
     selectedServiceMethod: null,
   };
 
-  renderPanelHeader = (name, info) => {
+  renderPanelHeader = (name, info, currentServiceId, serviceId) => {
+    const active = currentServiceId == serviceId ?
+      { size: "medium", color: "accent-4", side: "left" } : null;
     return (
-      <Box pad="small">
-        <Box direction="row" justify="between">
-          <Text size="large" weight="bold">{name}</Text>
-          <PlainButton icon={<FormDown />} />
+      <Box pad="small" >
+        <Box border={active} pad={{ left: 'small' }}>
+          <Box direction="row" justify="between">
+            <Text size="large" weight="bold">{name}</Text>
+            <PlainButton icon={<FormDown />} />
+          </Box>
+          <Text>{info}</Text>
         </Box>
-        <Text>{info}</Text>
-      </Box>
+      </Box >
     )
   };
 
   renderListOfMethods = (service) => {
     const { id, name, info, methods } = service;
     const serviceId = id;
-    const views = methods.map((method, index) =>
+
+    return methods.map((method, index) =>
       <Box key={index} background="light-1">
         <Button fill hoverIndicator onClick={() => this.props.onSelectServiceMethod({ serviceId, name, info, method })}>
           <Box pad='small'>
@@ -36,20 +42,20 @@ export default class index extends Component {
           </Box>
         </Button>
       </Box>)
-    return views;
   }
 
   renderServiceItem = () => {
-    const { services } = this.props;
-    const views = services.map((item, index) =>
+    const { services, workflow } = this.props;
+    const { appliedMethods } = workflow;
+    const currentNodeId = workflow.currentNode.id;
+    const currentServiceId = appliedMethods[currentNodeId].serviceId;
+    return services.map((item, index) =>
       <AccordionPanel
         key={index}
-        header={this.renderPanelHeader(item.name, item.info)}
+        header={this.renderPanelHeader(item.name, item.info, currentServiceId, item.id)}
       >
         {this.renderListOfMethods(item)}
       </AccordionPanel>)
-
-    return views;
   }
 
   render() {
@@ -66,5 +72,8 @@ export default class index extends Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  workflow: state.workflow
+});
 
-
+export default connect(mapStateToProps)(index);
