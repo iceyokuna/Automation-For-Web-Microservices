2951bb2098b80bd5c85f6d7d07c460e33d76b902
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-10 17:00:56 +0700

    [UPDATE] change debug

--- WebAutomationServer/WebAutomationServer/settings.py
+++ WebAutomationServer/WebAutomationServer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e3b9g=xi+th4cv-$%$a#l((cn1&)9g2wiv+aj_^xk$b+kb0&$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
