2e34ea674cc7ae5c2ca67378805e484c08475b22
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-05-23 17:33:44 +0700

    Create Modal component || Show method information

--- Client/src/pages/service_info/index.js
+++ Client/src/pages/service_info/index.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { connect } from 'react-redux'
 import {
   Box, Button, TextArea,
   TextInput, FormField,
-  Text,
+  Text, Select
 } from 'grommet';
 import { withRouter } from 'react-router-dom';
 import { Row, Col } from 'react-flexbox-grid';
+import { colors } from 'theme';
+import Modal from 'components/modal';
+
+const requestTypeOptions = ["GET", "POST", "PUT", "DELETE", "PATCH"];
+const interfacePlaceholder = `{
+  "date": {
+    "type": "string",
+    "elementType": "input"
+  }
+}`;
 
 const ServiceInfo = (props) => {
   const { currentService } = props.userServices;
@@ -17,30 +27,138 @@ const ServiceInfo = (props) => {
   const [serviceName, setServiceName] = useState(currentService.name);
   const [serviceInfo, setServiceInfo] = useState(currentService.info);
   // const [serviceUrl, setServiceUrl] = useState(currentService.path);
+  const [showMethodDialog, setShowMethodDialog] = useState(false);
+  const [currentMethod, setCurrentMethod] = useState(null);
+  const [methodName, setMethodName] = useState("");
+  const [methodInfo, setMethodInfo] = useState("");
+  const [methodUrl, setMethodUrl] = useState("");
+  const [methodType, setMethodType] = useState("");
+  const [methodInputInterface, setMethodInputInterface] = useState("");
+  const [methodOutputInterface, setMethodOutputInterface] = useState("");
+
+  useEffect(() => {
+    if (currentMethod != null) {
+      setMethodName(currentMethod.name);
+      setMethodInfo(currentMethod.info);
+      setMethodUrl(currentMethod.path || "/method#77");
+      setMethodType(currentMethod.methodType || "GET");
+      setMethodInputInterface(JSON.stringify(currentMethod.input_interface, null, 2));
+      setMethodOutputInterface(JSON.stringify(currentMethod.output_interface, null, 2));
+    }
+
+  }, [currentMethod])
+
+  const onSelectMethod = (method) => {
+    setCurrentMethod(method);
+    setShowMethodDialog(true);
+  }
+
+  function renderServiceInfo() {
+    return (
+      <Fragment>
+        <Text margin={{ bottom: 'small' }} size="large" weight="bold">Service's information</Text>
+        <Row>
+          <Col xs={12} sm={12} md={6} lg={6} >
+            <FormField>
+              <TextInput placeholder="Service name" size="small"
+                onChange={e => setServiceName(e.target.value)} value={serviceName} />
+            </FormField>
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={6} >
+            <FormField>
+              <TextInput placeholder="What does this service do ?" size="small"
+                onChange={e => setServiceInfo(e.target.value)} value={serviceInfo} />
+            </FormField>
+          </Col>
+        </Row>
+      </Fragment>);
+  }
+
+  function renderMethodList() {
+    const { methods } = currentService;
+    return (
+      <Fragment>
+        <Text margin={{ bottom: 'small' }} size="large" weight="bold">Service's methods</Text>
+        <table>
+          <tr>
+            <th>Method name</th>
+            <th>URL's endpoint</th>
+            <th>Request type</th>
+          </tr>
+          {methods.map((item, index) =>
+            <tr className="service" onClick={() => onSelectMethod(item)}>
+              <td>{item.name}</td>
+              <td>{item.info}</td>
+              <td>{item.methodType || "GET"}</td>
+            </tr>
+          )}
+        </table>
+      </Fragment>
+    )
+  }
+
+  function renderMethodInfo() {
+    return (
+      <Fragment>
+        <Row >
+          <Col xs={12} md={5} lg={5}>
+            <FormField>
+              <TextInput placeholder="Method name" size="small"
+                onChange={e => setMethodName(e.target.value)} value={methodName} />
+            </FormField>
+          </Col>
+          <Col xs={12} md={7} ld={7} >
+            <FormField>
+              <TextInput placeholder="What does this method do ?" size="small"
+                onChange={e => setMethodInfo(e.target.value)} value={methodInfo} />
+            </FormField>
+          </Col>
+
+          <Col xs={12} md={8} lg={8}>
+            <FormField>
+              <TextInput placeholder="URL's path e.g. /news/techologies" size="small"
+                onChange={e => setMethodUrl(e.target.value)} value={methodUrl} />
+            </FormField>
+          </Col>
+          <Col xs={12} md={4} lg={4}>
+            <FormField>
+              <Select
+                id="methodType"
+                name="methodType"
+                placeholder="GET"
+                value={methodType}
+                options={requestTypeOptions}
+                onChange={e => setMethodType(e.target.value)}
+              />
+            </FormField>
+          </Col>
+        </Row>
+
+        <Row style={{ height: 200 }}>
+          <Col xs={12} md={6} lg={6}>
+            <TextArea fill
+              value={methodInputInterface} placeholder={interfacePlaceholder}
+              onChange={e => setMethodInputInterface(e.target.value)} />
+          </Col>
+
+          <Col xs={12} md={6} lg={6}>
+            <TextArea fill
+              value={methodOutputInterface} placeholder={interfacePlaceholder}
+              onChange={e => setMethodOutputInterface(e.target.value)} />
+          </Col>
+        </Row>
+      </Fragment>
+    );
+  }
 
   return (
-    <Box gap="small" pad="small">
-
-      <Text size="medium" weight="bold">Service's information</Text>
-      <Row>
-        <Col xs={12} sm={12} md={6} lg={6} >
-          <FormField>
-            <TextInput placeholder="Service name" size="small"
-              onChange={e => setServiceName(e.target.value)} value={serviceName} />
-          </FormField>
-        </Col>
-        <Col xs={12} sm={12} md={6} lg={6} >
-          <FormField>
-            <TextInput placeholder="What does this service do ?" size="small"
-              onChange={e => setServiceInfo(e.target.value)} value={serviceInfo} />
-          </FormField>
-        </Col>
-      </Row>
-
-      {/* <FormField>
-        <TextInput placeholder="Service 's URL e.g. https://myservice.com/api/" size="small"
-          onChange={this.onChangeServiceUrl} value={serviceUrl} />
-      </FormField>  */}
+    <Box gap="medium" pad="small" margin={{ top: "medium" }}>
+      {renderServiceInfo()}
+      {renderMethodList()}
+      <Modal show={showMethodDialog}
+        width="650px"
+        onCloseModal={() => setShowMethodDialog(false)}
+        header="Method's information" component={renderMethodInfo()} />
     </Box>
   );
 }
