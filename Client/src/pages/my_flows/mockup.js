ed579e9ae0876a289af30b25ce37bb65842b9204
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-05-26 21:13:06 +0700

    Edit mockup data

--- Client/src/pages/my_flows/mockup.js
+++ Client/src/pages/my_flows/mockup.js
@@ -2,7 +2,7 @@ export const myFlows = [
   { name: 'IC Meeting', description: 'Application for helping reserve meeting date of my company', owner: 'Me', status: 'Active' },
   { name: 'Student Registration', description: 'Application description', owner: 'Me', status: 'Active' },
   { name: 'Q/A for KMITL students', description: 'Application description', owner: 'Me', status: 'Active' },
-  { name: 'Employee Resistration', description: 'Application description', owner: 'Me', status: 'Active' },
-  { name: 'KMITL supervisor meeting', description: 'Application description', owner: 'Me', status: 'Active' },
-  { name: 'Smart room', description: 'Application description', owner: 'Me', status: 'Active' },
+  { type: 'group', name: 'Employee Resistration', description: 'Application description', owner: 'Me', status: 'Active' },
+  { type: 'group', name: 'KMITL supervisor meeting', description: 'Application description', owner: 'Me', status: 'Active' },
+  { type: 'group', name: 'Smart room', description: 'Application description', owner: 'Me', status: 'Active' },
 ]
