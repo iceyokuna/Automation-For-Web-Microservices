2c1222c76d4d353b733254619d58e48f4dcacb45
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-05-26 19:08:10 +0700

    Update url to http host

--- Client/src/_constants/global.constants.js
+++ Client/src/_constants/global.constants.js
@@ -7,20 +7,20 @@ export const globalConstants = {
   USER_LOGOUT_URL: `${developmentServer}:8003/api/logout`,
   USER_WORKFLOW_URL: `${developmentServer}:8003/api/workflow`,
 
-  GET_ALL_SERVICES_URL: `${developmentServer}:8004/api/all_services`,
+  GET_ALL_SERVICES_URL: `${developmentServer}:8004/api/all_services/`,
   GET_ALL_INBOX_TASKS_URL: `https://5c8f65998447f30014cb826b.mockapi.io/api/endUserTasks`,
   GET_ALL_NOTIFICATIONS_URL: `https://5c8f65998447f30014cb826b.mockapi.io/api/notifications`,
-  GET_ALL_USER_SERVICES_URL: `${developmentServer}:8004/api/all_user_service`,
+  GET_ALL_USER_SERVICES_URL: `${developmentServer}:8004/api/all_user_service/`,
 
-  COLLABORATORS_URL: `${developmentServer}:8004/api/collaborator`,
-  SET_FCM_TOKEN_URL: `${developmentServer}:8004/api/fcm_token`,
+  COLLABORATORS_URL: `${developmentServer}:8003/api/collaborator/`,
+  SET_FCM_TOKEN_URL: `${developmentServer}:8003/api/fcm_token/`,
 
-  CREATE_USER_SERVICE_URL: `${developmentServer}:8003/api/service/add`,
-  DELETE_USER_SERVICE_URL: `${developmentServer}:8003/api/user_service`,
+  CREATE_USER_SERVICE_URL: `${developmentServer}:8004/api/service/add`,
+  DELETE_USER_SERVICE_URL: `${developmentServer}:8004/api/user_service/`,
 
-  USER_METHOD_URL: `${developmentServer}:8003/api/user_method`,
+  USER_METHOD_URL: `${developmentServer}:8003/api/user_method/`,
 
-  DELETE_COLLABORATORS_URL: `${developmentServer}:8003/api/collaborator/delete`,
+  DELETE_COLLABORATORS_URL: `${developmentServer}:8003/api/collaborator/delete/`,
 
 
 };
