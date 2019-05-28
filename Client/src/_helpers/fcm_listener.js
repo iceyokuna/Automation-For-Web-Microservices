0ebd37f7fe6dc0778e19ee019d49839873c60aca
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-05-28 17:48:07 +0700

    Bug fix

--- Client/src/_helpers/fcm_listener.js
+++ Client/src/_helpers/fcm_listener.js
@@ -7,6 +7,7 @@ import { getUserToken } from '_helpers'
 export function applyFCMListener(store) {
   messaging.onMessage(payload => {
     toast.info(payload.notification.title);
+    console.log({payload});
     store.dispatch(notificationActions.addNewNotification(payload));
   }, err => {
     console.error(err)
