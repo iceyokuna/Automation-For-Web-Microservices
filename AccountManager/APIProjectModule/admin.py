91b659de4b5b2d797231dfd8e2c8de81aaa6d7e9
treesakul <treesakul@hotmail.com>
2019-05-29 19:36:19 +0700

    Update admin.py

--- AccountManager/APIProjectModule/admin.py
+++ AccountManager/APIProjectModule/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Workflow, Collaborator
+from .models import Workflow, Collaborator,Log
 # Register your models here.
 
 admin.site.register(Workflow)
