34ee26f4b5c8a6744d416229f56ec1c7a20b360a
Iceyo Kuna <iceyokuna@hotmail.com>
2019-05-31 17:22:16 +0700

    [DEPLOY] allow host origin google service

--- GoogleService/GoogleService/settings.py
+++ GoogleService/GoogleService/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hm-82^+eqzh5+c#!_ywmz3pi_h6(&t%-r3o1^h61+*(3ye1ksv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'GoogleService.urls'
 
