e767991c7ac048a0c04890c1514f8384a4258e99
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-06-04 23:53:25 +0700

    Refactor global url

--- Client/src/_constants/global.constants.js
+++ Client/src/_constants/global.constants.js
@@ -5,32 +5,26 @@ const workflowEngineUrl = "http://127.0.0.1:8000";
 // const googleServiceUrl = "http://127.0.0.1:8001";
 const googleServiceUrl = "https://c3160663.ngrok.io";
 
+const accountManagerUrl = `${developmentServer}:8003`;
+const serviceManagerUrl = `${developmentServer}:8004`;
+
 export const globalConstants = {
-  USER_REGISTER_URL: `${developmentServer}:8003/api/register`,
-  USER_LOGIN_URL: `${developmentServer}:8003/api/login`,
-  USER_LOGOUT_URL: `${developmentServer}:8003/api/logout`,
-  USER_WORKFLOW_URL: `${developmentServer}:8003/api/workflow`,
+  USER_REGISTER_URL: `${accountManagerUrl}/api/register`,
+  USER_LOGIN_URL: `${accountManagerUrl}/api/login`,
+  USER_LOGOUT_URL: `${accountManagerUrl}/api/logout`,
+  USER_WORKFLOW_URL: `${accountManagerUrl}/api/workflow`,
+  DELETE_COLLABORATORS_URL: `${accountManagerUrl}/api/collaborator/delete/`,
+  WORKFLOW_LOG_URL: `${accountManagerUrl}/api/log/`,
+  COLLABORATORS_URL: `${accountManagerUrl}/api/collaborator/`,
+  SET_FCM_TOKEN_URL: `${accountManagerUrl}/api/fcm_token/`,
 
-  GET_ALL_SERVICES_URL: `${developmentServer}:8004/api/all_services/`,
-  GET_ALL_INBOX_TASKS_URL: `https://5c8f65998447f30014cb826b.mockapi.io/api/endUserTasks`,
-  GET_ALL_NOTIFICATIONS_URL: `https://5c8f65998447f30014cb826b.mockapi.io/api/notifications`,
-  GET_ALL_USER_SERVICES_URL: `${developmentServer}:8004/api/all_user_service/`,
-
-  WORKFLOW_LOG_URL: `${developmentServer}:8003/api/log/`,
-
-  COLLABORATORS_URL: `${developmentServer}:8003/api/collaborator/`,
-  SET_FCM_TOKEN_URL: `${developmentServer}:8003/api/fcm_token/`,
-
-  UPDATE_USER_SERVICE_URL: `${developmentServer}:8004/api/user_service/`,
-
-  CREATE_USER_SERVICE_URL: `${developmentServer}:8004/api/service/add`,
-  DELETE_USER_SERVICE_URL: `${developmentServer}:8004/api/user_service/`,
-
-  USER_METHOD_URL: `${developmentServer}:8004/api/user_method/`,
-
-  DELETE_COLLABORATORS_URL: `${developmentServer}:8003/api/collaborator/delete/`,
+  GET_ALL_SERVICES_URL: `${serviceManagerUrl}/api/all_services/`,
+  GET_ALL_USER_SERVICES_URL: `${serviceManagerUrl}/api/all_user_service/`,
+  UPDATE_USER_SERVICE_URL: `${serviceManagerUrl}/api/user_service/`,
+  CREATE_USER_SERVICE_URL: `${serviceManagerUrl}/api/service/add`,
+  DELETE_USER_SERVICE_URL: `${serviceManagerUrl}/api/user_service/`,
+  USER_METHOD_URL: `${serviceManagerUrl}/api/user_method/`,
 
   GET_PREVIEW_INPUT_FORM_URL: `${workflowEngineUrl}/preview/`,
-
   GOOGLE_DOC_API_CREATE_URL: `${googleServiceUrl}/api/docs/create/`,
 };
