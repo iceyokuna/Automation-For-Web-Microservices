4f97d415a54b8c4953c7015b1628c89b7e5f8d13
Phat Thaveepholcharoen <overtone00445@gmail.com>
2019-06-05 17:44:59 +0700

    Add preview form to execution page

--- Client/src/pages/execute_flow/index.js
+++ Client/src/pages/execute_flow/index.js
@@ -13,6 +13,7 @@ import MonitorDiagram from 'components/monitor_diagram';
 import Spinner from 'react-spinkit';
 import { colors } from 'theme';
 import { GoogleLogin } from 'react-google-login';
+import $ from 'jquery';
 
 class ExecuteFlow extends Component {
 
@@ -26,6 +27,7 @@ class ExecuteFlow extends Component {
       currentFormHtml: null,
       currentFormCss: null,
       currentFormJs: null,
+      showInspection: false,
     }
   }
 
@@ -51,6 +53,22 @@ class ExecuteFlow extends Component {
     //         currentFormJs: "",
     //     });
     // }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { workflowMonitor, } = this.props;
+    const { formInputValues } = workflowMonitor;
+    this.assignValuesToInputForm(formInputValues);
+  }
+
+
+  assignValuesToInputForm = (formInputValues) => {
+    if (formInputValues == null) return;
+    // console.log({ formInputValues });
+    Object.keys(formInputValues).forEach((id, index) => {
+      console.log({ id });
+      $(`#${id}`).val(formInputValues[id].value);
+    })
   }
 
   getPreviousForm = () => {
@@ -94,6 +112,16 @@ class ExecuteFlow extends Component {
     ));
   }
 
+  onClickElement = (elementId) => {
+    this.setState({
+      showInspection: true,
+      elementToInspect: elementId
+    });
+
+    const { dispatch, currentFlow } = this.props;
+    dispatch(monitorActions.getInputForm(currentFlow.id, elementId));
+  }
+
   onAcceptWaitingStatus = () => {
     const { dispatch, history, } = this.props;
     dispatch(workflowActions.closeWaitingDialog());
@@ -102,6 +130,10 @@ class ExecuteFlow extends Component {
 
   onOpenLogs = () => {
     this.props.dispatch(monitorActions.toggleDock());
+  }
+
+  onCloseInspection = () => {
+    this.setState({ showInspection: !this.state.showInspection });
   }
 
   responseGoogleSignin = (response) => {
@@ -144,6 +176,45 @@ class ExecuteFlow extends Component {
     }
   }
 
+  renderElementInspection = () => {
+    const { showInspection, elementToInspect } = this.state;
+    const { currentFlow, workflowMonitor, } = this.props;
+    const { loadingInputForm } = workflowMonitor;
+    const { formInputValues } = workflowMonitor;
+
+    if (!showInspection || currentFlow.generatedForms.length == 0) return null;
+
+    const currentForm = currentFlow.generatedForms.find(
+      item => item.taskId === elementToInspect
+    )
+
+    // Element doesn't have a form
+    if (currentForm == null) return null;
+    if (formInputValues == null) return null;
+
+    const { formHtml, formCss, } = currentForm.forms.inputForm;
+    return (
+      <Modal header="Inspection" show={showInspection}
+        onCloseModal={this.onCloseInspection}>
+        {loadingInputForm ? (
+          <Box align="center" pad='small'
+            justify="center" height="50px">
+            <Spinner
+              fadeIn="half"
+              name="ball-scale-multiple" color={colors.brand} />
+          </Box>
+        ) : (
+            <Box animation={{ type: 'fadeIn', duration: 500 }}>
+              <Style css={formCss} />
+              <Text>* Received inputs</Text>
+              <div dangerouslySetInnerHTML={{ __html: formHtml }}
+                style={{ pointerEvents: 'none', opacity: 0.7, }} />
+            </Box>
+          )}
+      </Modal>
+    );
+  }
+
   render() {
     const { currentFormCss, currentFormHtml, } = this.state;
     const { dispatch, workflow, history, } = this.props;
@@ -151,16 +222,15 @@ class ExecuteFlow extends Component {
 
     return (
       <FillParent>
-
+        {this.renderElementInspection()}
         {this.renderLoadingFormModal()}
-
         <OpenDock icon={<CaretUp color="#fff" />}
           color="accent-4" primary plain={false}
           data-tip="Workflow logs"
           onClick={this.onOpenLogs} />
 
         <DockContainer >
-          <MonitorDiagram height="350px" />
+          <MonitorDiagram height="350px" onClickElement={this.onClickElement} />
         </DockContainer>
 
         <Modal show={showWaitingDialog} header="Execution status"
@@ -199,8 +269,10 @@ const mapStateToProps = (state) => {
   return {
     workflow: state.workflow,
     authentication: state.authentication,
+    currentFlow: state.workflowMyFlows.currentFlow,
     currentWorkflowId: state.workflowMyFlows.currentFlow.id,
     socket: state.socket,
+    workflowMonitor: state.workflowMonitor,
   }
 }
 
