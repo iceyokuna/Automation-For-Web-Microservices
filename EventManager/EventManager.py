66418b481ff7d5ed7e28469b2dcc123375d87767
Iceyo Kuna <iceyokuna@hotmail.com>
2019-06-06 03:29:25 +0700

    [DEMO] FIX PORT

--- EventManager/EventManager.py
+++ EventManager/EventManager.py
@@ -73,5 +73,5 @@ if __name__ == '__main__':
     worker.start()
 
     #start app (REST API)
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='127.0.0.1',port='5000', debug=True, threaded=True)
     
