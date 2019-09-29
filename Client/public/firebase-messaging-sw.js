42cf7278ce416f3f01fad17ccca03dfb20c1004d
Phat <overtone00445@gmail.com>
2019-09-29 19:33:37 +0700

    ปิด fcm ไว้ก่อน

--- Client/public/firebase-messaging-sw.js
+++ Client/public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.0.2/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/6.0.2/firebase-messaging.js');
 
-firebase.initializeApp({
-    messagingSenderId: "807661190255",
-});
-const messaging = firebase.messaging();
+// firebase.initializeApp({
+//     messagingSenderId: "807661190255",
+// });
+// const messaging = firebase.messaging();
