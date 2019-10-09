cee05e5931be8020687c867e835f97a1f5533d84
Phat Thaveepholcharoen <phat.t@wongnai.com>
2019-10-09 11:48:43 +0700

    ปิด ask notification permission

--- Client/src/pages/home/index.js
+++ Client/src/pages/home/index.js
@@ -1,58 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { global, FillParent } from 'style'
+import { global, FillParent } from "style";
 
-import AppBar from 'components/app_bar';
-import DropMenuInline from 'components/drop_menu_inline'
-import SideBar from 'components/sidebar';
+import AppBar from "components/app_bar";
+import DropMenuInline from "components/drop_menu_inline";
+import SideBar from "components/sidebar";
 
-import { Box, } from 'grommet';
+import { Box } from "grommet";
 
-import FlowDetail from 'pages/flow_detail'
-import CreateForm from 'pages/create_form'
-import NotFound from 'pages/not_found'
-import MyFlows from 'pages/my_flows';
-import CreateFlow from 'pages/create_flow'
-import Workflow from 'pages/workflow'
-import MyTasks from 'pages/my_tasks'
-import InboxTaskDetail from 'pages/inbox_task_detail'
-import Services from 'pages/services';
-import ServiceAdd from 'pages/service_add'
+import FlowDetail from "pages/flow_detail";
+import CreateForm from "pages/create_form";
+import NotFound from "pages/not_found";
+import MyFlows from "pages/my_flows";
+import CreateFlow from "pages/create_flow";
+import Workflow from "pages/workflow";
+import MyTasks from "pages/my_tasks";
+import InboxTaskDetail from "pages/inbox_task_detail";
+import Services from "pages/services";
+import ServiceAdd from "pages/service_add";
 
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import Media from 'react-media';
+import Media from "react-media";
 
-import { history, askForPermissioToReceiveNotifications } from '_helpers';
-import { notificationServices } from 'services'
-import ServiceInfo from 'pages/service_info';
+import { history } from "_helpers";
+import { notificationServices } from "services";
+import ServiceInfo from "pages/service_info";
 
 export default class Home extends Component {
   state = {
-    showMenuBar: false,
-  }
+    showMenuBar: false
+  };
 
-  componentDidMount() {
-    askForPermissioToReceiveNotifications().then(fcmToken => {
-      console.log("fcmToken = " + fcmToken);
-      notificationServices.setFCMToken(fcmToken).then(
-        res => {
-          console.log("Set fcmToken");
-        }
-      )
-    }).catch(err => {
-      console.error(err);
-    });
-  }
+  // componentDidMount() {
+  //   askForPermissioToReceiveNotifications().then(fcmToken => {
+  //     console.log("fcmToken = " + fcmToken);
+  //     notificationServices.setFCMToken(fcmToken).then(
+  //       res => {
+  //         console.log("Set fcmToken");
+  //       }
+  //     )
+  //   }).catch(err => {
+  //     console.error(err);
+  //   });
+  // }
 
-
-  toggleMenubar = (e) => {
+  toggleMenubar = e => {
     this.setState({ showMenuBar: !this.state.showMenuBar });
-  }
+  };
 
-  navigateTo = (pathName) => {
+  navigateTo = pathName => {
     history.push(pathName);
-  }
+  };
 
   renderRoutes = () => {
     const { match } = this.props;
@@ -61,7 +60,10 @@ export default class Home extends Component {
         <Switch>
           <Route exact path={"/my_flows"} component={MyFlows} />
           <Route path={"/my_flows/create"} component={CreateFlow} />
-          <Route path={"/my_flows/:flow_id/edit_diagram"} component={Workflow} />
+          <Route
+            path={"/my_flows/:flow_id/edit_diagram"}
+            component={Workflow}
+          />
           <Route path={"/my_flows/:flow_id"} component={FlowDetail} />
           <Route exact path={"/my_tasks"} component={MyTasks} />
           <Route exact path={"/design_form"} component={CreateForm} />
@@ -75,36 +77,43 @@ export default class Home extends Component {
         </Switch>
       </div>
     );
-  }
-
+  };
 
   render() {
     const { showMenuBar } = this.state;
     return (
       <Box flex fill="vertical">
-        <AppBar {...this.props} showMenuBar={showMenuBar}
-          onToggleMenu={this.toggleMenubar} />
+        <AppBar
+          {...this.props}
+          showMenuBar={showMenuBar}
+          onToggleMenu={this.toggleMenubar}
+        />
         <Media query="(min-width: 599px)">
           {matches =>
             matches ? (
               <FillParent style={{ paddingTop: 60 }}>
                 <Box fill direction="row">
-                  <SideBar showMenuBar={showMenuBar}
-                    onSelectMenu={(pathName) => this.navigateTo(pathName)} {...this.props} />
+                  <SideBar
+                    showMenuBar={showMenuBar}
+                    onSelectMenu={pathName => this.navigateTo(pathName)}
+                    {...this.props}
+                  />
                   {this.renderRoutes()}
                 </Box>
-
               </FillParent>
             ) : (
-                <FillParent style={{ paddingTop: 60 }}>
-                  <DropMenuInline showMenuBar={showMenuBar}
-                    onSelectMenu={(pathName) => this.navigateTo(pathName)} {...this.props} />
-                  {this.renderRoutes()}
-                </FillParent>
-              )
+              <FillParent style={{ paddingTop: 60 }}>
+                <DropMenuInline
+                  showMenuBar={showMenuBar}
+                  onSelectMenu={pathName => this.navigateTo(pathName)}
+                  {...this.props}
+                />
+                {this.renderRoutes()}
+              </FillParent>
+            )
           }
         </Media>
-      </Box >
-    )
+      </Box>
+    );
   }
 }
